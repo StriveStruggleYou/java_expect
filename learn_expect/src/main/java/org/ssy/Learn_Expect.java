9d17c9316b55336f0cc05a0f74978669caf0211a
chendekai <chendekai@qipeng.com>
2018-07-02 21:50:16 +0800

    提交基础信息

---
+++ learn_expect/src/main/java/org/ssy/Learn_Expect.java
@@ -0,0 +1,10 @@
+package org.ssy;
+
+/**
+ * Created by manager on 2018/7/2.
+ */
+public class Learn_Expect {
+
+  
+
+}
--- src/main/java/org/ssy/expect/LearnExpect.java
+++
@@ -1,8 +0,0 @@
-package org.ssy.expect;
-
-/**
- * Created by manager on 2018/7/2.
- */
-public class LearnExpect {
-
-}
